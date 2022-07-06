aee462a1a3e2d543faba460ed5081664caec4ff1
Igor <igorfontenelletexeira@gmail.com>
2022-07-06 18:31:10 -0300

    Primeiro get feito, post em desenvolvimento

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 
 dotenv.config();
 
 const server = express();
 server.use([express.json(), cors()]);
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
 
-server.get("/", (request, response) => {
-    response.send("Hello World");
+function mongoConection() {
+    mongoClient.connect().then(() => {
+        db = mongoClient.db("EletroStoreDB"); // Conectamos à coleção "meu_lindo_banco".
+      });
+}
+mongoConection();
+
+server.get("/main", async (request, response) => {
+    const items = await db.collection("items").find().toArray();
+    
+
+
+    response.send(items);
 });
 
 const PORT = process.env.PORT || 4000;
