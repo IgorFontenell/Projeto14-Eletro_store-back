a5f96c040b8659a1c79bba56f8cf408ed6c1194d
Igor <igorfontenelletexeira@gmail.com>
2022-07-06 21:22:31 -0300

    Get dos items e post de cadastro dos itens pronto

--- src/index.js
+++ src/index.js
@@ -1,30 +1,58 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import joi from "joi";
 import { MongoClient } from "mongodb";
 
 dotenv.config();
 
 const server = express();
-server.use([express.json(), cors()]);
+server.use(express.json());
+server.use(cors());
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 function mongoConection() {
     mongoClient.connect().then(() => {
-        db = mongoClient.db("EletroStoreDB"); // Conectamos à coleção "meu_lindo_banco".
+        db = mongoClient.db("EletroStoreDB"); 
       });
 }
 mongoConection();
-
+//Necessário uma verificação se é o usuário mesmo ainda a ser feita!
 server.get("/main", async (request, response) => {
+
+
+
+    
     const items = await db.collection("items").find().toArray();
     
-
-
     response.send(items);
 });
 
+server.post("/stock", async (request, response) => {
+    const item = request.body;
+    const itemSchema = joi.object({
+        name: joi.string().required(),
+        price: joi.number().required(),
+        description: joi.string().required(),
+        class: joi.string().required(),
+        img: joi.string().uri().required()
+    });
+
+    const validation = itemSchema.validate(item);
+    if(!validation) {
+        response.status(400).send("Item mal cadastrado!");
+        return;
+    }
+    await db.collection("items").insertOne({
+        name: item.name,
+        price: item.price,
+        description: item.description,
+        class: item.class
+    });
+    response.status(201).send("Item cadastrado com sucesso!");
+
+});
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log("Servidor rodando..."));
